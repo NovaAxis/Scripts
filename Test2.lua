37ac50319107ab72556d12e57265010312d3d1a2
NovaAxis <analyzpodr@gmail.com>
2025-10-18 06:15:23 +0500

    Update Test2.lua

--- Test2.lua
+++ Test2.lua
@@ -1,1 +1,7 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+local Window = WindUI:CreateWindow({
+    Title = "💫 NovaAxis",
+    Icon = "sparkles", -- lucide icon. optional
+    Author = "NovaAxis", -- optional
+})
