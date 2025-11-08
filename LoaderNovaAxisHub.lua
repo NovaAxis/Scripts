df82eb73e35a60bd3aca62757a60fc4995c1fa80
NovaAxis <analyzpodr@gmail.com>
2025-11-08 05:14:56 +0500

    Update LoaderNovaAxisHub.lua

--- LoaderNovaAxisHub.lua
+++ LoaderNovaAxisHub.lua
@@ -1,8 +1,9 @@
 local games = {
-    [994732206] = "Blox%20Fruits/Main.lua",
     [8243510333] = "Steal%20A%20Femboy/Main.lua",
-    [8170321325] = "Steal%20A%20Cat/Main.lua",
     [8483837377] = "Steal%20a%2099%20Nights/Main.lua",
+    [8170321325] = "Steal%20A%20Cat/Main.lua",
+    [8166233521] = "Survive%20On%20A%20Raft/Main.lua"
+    [994732206] = "Blox%20Fruits/Main.lua",
     [7738927312] = "Build%20A%20Bunker/Main.lua"
 }
 
