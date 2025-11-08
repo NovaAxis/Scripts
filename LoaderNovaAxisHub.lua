10c32c8aac3ba5ec1d651faa62bd68cad2be62a4
NovaAxis <analyzpodr@gmail.com>
2025-11-08 05:06:00 +0500

    Update LoaderNovaAxisHub.lua

--- LoaderNovaAxisHub.lua
+++ LoaderNovaAxisHub.lua
@@ -1,1 +1,12 @@
+local games = {
+    [994732206] = "Blox%20Fruits/Main.lua",
+    [8243510333] = "Steal%20A%20Femboy/Main.lua",
+    [8170321325] = "Steal%20A%20Cat/Main.lua",
+    [8483837377] = "Steal%20a%2099%20Nights/Main.lua",
+    [7738927312] = "Build%20A%20Bunker/Main.lua"
+}
 
+local url = games[game.GameId]
+if url then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaAxis/Scripts/refs/heads/main/Game/"..url))()
+end
